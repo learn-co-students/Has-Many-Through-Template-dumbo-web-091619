5c0b3fb95caa85dafda218f5aecb07d186152e7f
github username <github email address>
2019-09-22 19:48:12 -0400

    Done.

--- lib/Model2.rb
+++ lib/Model2.rb
@@ -1,5 +1,16 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Student
 
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
